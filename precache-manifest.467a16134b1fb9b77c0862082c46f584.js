faad8a5ef7f8a9237fd967eebdf68fed6788e670
Peter Tshiani <tshianipeter@gmail.com>
2019-10-14 13:38:22 +0200

    Updates

---
+++ precache-manifest.467a16134b1fb9b77c0862082c46f584.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a73c415c29e7a3aa78df53d96928b2",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "6a40bab1c9a3f043978e",
+    "url": "/todo-list/static/css/main.dbe491e6.chunk.css"
+  },
+  {
+    "revision": "224aa31f6c8c2d8aedc3",
+    "url": "/todo-list/static/js/2.7bd9c236.chunk.js"
+  },
+  {
+    "revision": "6a40bab1c9a3f043978e",
+    "url": "/todo-list/static/js/main.980e5e4b.chunk.js"
+  },
+  {
+    "revision": "8561f048c21294f6b6d5",
+    "url": "/todo-list/static/js/runtime~main.c8915f82.js"
+  }
+]);
